a2c9dceb94a5d5fdff2181dc34497ed377bac8ca
Gegam Arakelyan <gexam96@MBP-Gegam.Dlink>
2021-07-13 01:24:20 +0400

    some changes

--- index.js
+++ index.js
@@ -12,33 +12,9 @@ mongoose.connect('mongodb+srv://Gegham:g13131213@cluster0.8hk1c.mongodb.net/firs
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err))
 
-const UsersSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    }
-});
+
 
 const Users = mongoose.model('Users', UsersSchema);
-
-app.post('/', (req, res) => {
-    Users.create({
-        name: name,
-        age: age
-    })
-        .then((user) => res.send(user))
-        .catch(err => res.send(err));
-});
-
-app.get('/', (req, res) => {
-    Users.find()
-        .then(users => res.send(users))
-        .catch(err => res.send(err));
-})
 
 const server = createServer(app);
 server.listen(3000, () => console.log('Server connected'))
